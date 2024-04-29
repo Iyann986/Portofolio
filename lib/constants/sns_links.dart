d041415fe504e438c9dcc7ad71620070fc00eeb7
Iyann986 <hisdiantonoktavian@gmail.com>
2024-04-29 14:47:43 +0700

    Merubah link instagram

--- lib/constants/sns_links.dart
+++ lib/constants/sns_links.dart
@@ -2,7 +2,7 @@ class SnsLinks {
   static const String github = "https://github.com/Iyann986";
   static const String linkedIn = "https://www.linkedin.com/in/oktavianh/";
   static const String facebook = "https://";
-  static const String instagram = "https://www.instagram.com/_iyann2/";
+  static const String instagram = "https://";
   static const String telegram = "https://";
   static const String resume =
       "https://drive.google.com/file/d/1cEMlkiYqWkumg-4bNJR1TpND8MPLQZ1g/view?usp=sharing";
